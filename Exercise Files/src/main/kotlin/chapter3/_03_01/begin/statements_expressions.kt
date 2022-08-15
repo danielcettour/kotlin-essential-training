7b94e9714eb9eb41cbdd7c19f69eeafac0ef7281
danielcettour <dcettour@jerarquicos.com>
2022-08-15 19:20:26 -0300

    strings, statements, if, when

--- Exercise Files/src/main/kotlin/chapter3/_03_01/begin/statements_expressions.kt
+++ Exercise Files/src/main/kotlin/chapter3/_03_01/begin/statements_expressions.kt
@@ -5,4 +5,42 @@ fun returnsNothing() {
 }
 
 fun main() {
+    val bigger = 1000
+    val smaller = 1
+
+    /**
+     * ternary operator doesn't exist en kotlin, we use if that returns a value
+     */
+
+    // won't compile
+    // val max = bigger > smaller ? bigger : smaller
+
+    //will compile
+    val max = if (bigger > smaller) bigger else smaller
+
+    /**
+     * when replaces switch in other languages
+     */
+    val age = 35
+    val boardGroup = when (age) {
+        in 0..18 -> "family"
+        in 65..200 -> "senior"
+        else -> "regular"
+    }
+    println("group: $boardGroup")
+
+    /**
+     * kotlin.Unit is the equivalent to void
+     */
+
+    val fromNothing = returnsNothing()
+    println("returns kotlin.Unit, equivalent to void in java: $fromNothing")
+
+    /**
+     * multiple assignments
+     */
+    val a = 100
+    val b = 101
+    // won't compile
+    //a = b = 98 *  /* in kotlin, the assignment operator is a statement, making this an error */
 }
--- Exercise Files/src/main/kotlin/chapter3/_03_02/begin/if_expression.kt
+++ Exercise Files/src/main/kotlin/chapter3/_03_02/begin/if_expression.kt
@@ -1,4 +1,37 @@
 package chapter3._03_02.begin
 
 fun main() {
+    val bigger = 101
+    val smaller = 1
+    val max = if (bigger > smaller) bigger else smaller
+
+    if(bigger>10) {
+        println("bigger than 10")
+    } else if (bigger > 100) {
+        println("bigger than 100") // bigger is 101 so it will enter the first if statement
+    } else {
+        println("Not too big")
+    }
+
+    val number = 121
+
+    // any branch of the if statement can return a different type value, so "bucket is of type Any"
+    val bucket: Any = if(number > 100) {
+        "alpha"
+    } else if(number < 90) {
+        println("less than 90")
+        2
+    } else if (number > 80) {
+        1
+    } else {
+        0
+    }
+    println("bucket = $bucket")
+
+    /**
+     * if returns a boolean, so next line is redundant
+     */
+    // val isGreater = if(number > 100) true else false
+    val isGreater = number > 100 // replaces the line above
+    println("is greater: $isGreater")
 }
