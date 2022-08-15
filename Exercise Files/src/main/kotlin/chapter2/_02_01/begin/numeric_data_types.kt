0268a7a374500f8a27a70339311772ef6c0328f7
danielcettour <dcettour@jerarquicos.com>
2022-08-15 16:00:54 -0300

    booleans, chars, numeric excersices done

--- Exercise Files/src/main/kotlin/chapter2/_02_01/begin/numeric_data_types.kt
+++ Exercise Files/src/main/kotlin/chapter2/_02_01/begin/numeric_data_types.kt
@@ -5,13 +5,13 @@ package chapter2._02_01.begin
  */
 fun main() {
     var count: Int = 0 // var is mutable, can change
-    val number: Int = 42 // immutable, best practice, use whenever possible
+    val number: Int = 42 // immutable, considered a best practice, use whenever possible. Legible, testable, thread safe.
 
     val otherNumber = 43 // inferred type
 
     val myLong: Long = 3000000000L // L for long
-    val myByte: Byte = 127 // four digits gives error
-    val myShort: Short = 32767 // 327670 out of range, gives error
+    val myByte: Byte = 127 // four digits gives compilation error
+    val myShort: Short = 32767 // 327670 out of range, gives compilation error
 
     val myDouble: Double = 98.6 // inferred
     val myFloat: Float = 12.2F // F for float
@@ -22,4 +22,5 @@ fun main() {
     val asFloat = myLong.toFloat()
 
     val maxInt = 2_147_483_648 // easier to read with underscores, equals 2147483648
+
 }
--- Exercise Files/src/main/kotlin/chapter2/_02_03/begin/boolean_and_char.kt
+++ Exercise Files/src/main/kotlin/chapter2/_02_03/begin/boolean_and_char.kt
@@ -11,12 +11,42 @@ fun secondOperand(): Boolean {
 }
 
 fun main() {
-//    demoBoolean()
+    demoBoolean()
 //    demoChar()
 }
 
 private fun demoBoolean() {
+    val willExcersice = false
+    val bigNumber = 1_000_000
+    val smallNumber = 2
+    println("is bigger: ${bigNumber > smallNumber}")
+
+    if(firstOperand() || secondOperand()) {
+        println("At least one operand is true")
+    }
+
+    if(firstOperand() && secondOperand()) {
+        // shortcut, the second operand will not evaluate
+        println("Both operands are true")
+    }
+
+    println("Will excersice= ${!willExcersice}")
 }
 
 private fun demoChar() {
+    /**
+     *  in kotlin, characters are not numbers.
+     *  kotlin never does implicit conversions
+     */
+    val letter: Char = 'A'
+    val tab = '\t'
+    val infinity = '\u221E'
+
+    println("letter: $letter")
+    println("tabbed $tab over")
+    println("infinity: $infinity")
+
+    val lineFeed = 10.toChar() // equals \n
+    println("line feed = $lineFeed next line")
+
 }
