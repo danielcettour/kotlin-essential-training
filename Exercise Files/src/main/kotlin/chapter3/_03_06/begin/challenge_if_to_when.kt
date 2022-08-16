9a33c20c60b24a60cf44a09e82a6e8901dbc24a9
danielcettour <dcettour@jerarquicos.com>
2022-08-15 21:10:33 -0300

    nulls, smart casting, loops, challenge

--- Exercise Files/src/main/kotlin/chapter3/_03_06/begin/challenge_if_to_when.kt
+++ Exercise Files/src/main/kotlin/chapter3/_03_06/begin/challenge_if_to_when.kt
@@ -1,12 +1,12 @@
 package chapter3._03_06.begin
 
 fun main() {
-    var cardPoints = 7_000
+    val cardPoints = 7_000
 
     // TODO: replace this if with a when
     // bonus points for using ranges!
 
-    val cardLevel: String = if (cardPoints >= 0 && cardPoints < 1000) {
+    /*val cardLevel: String = if (cardPoints >= 0 && cardPoints < 1000) {
         "pearl"
     } else if (cardPoints >= 1000 && cardPoints < 5_000) {
         "silver"
@@ -14,8 +14,15 @@ fun main() {
         "gold"
     } else {
         "platinum"
+    }*/
+
+    val cardLevel = when(cardPoints) {
+        in 0..999 -> "pearl"
+        in 1_000..4_999 -> "silver"
+        in 5_000..9_999 -> "gold"
+        else -> "platinum"
     }
 
-    val plural = if(cardPoints > 1 || cardPoints == 0) "s" else ""
+    val plural = if(cardPoints > 1) "s" else ""
     println("You have $cardPoints point$plural and are at the $cardLevel level.")
 }
