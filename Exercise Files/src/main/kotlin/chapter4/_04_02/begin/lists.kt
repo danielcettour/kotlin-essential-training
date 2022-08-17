e942fd5208ae51d68e8752e0a9e90ee0f6ac6196
danielcettour <dcettour@jerarquicos.com>
2022-08-17 16:32:00 -0300

    arrays, collections, lists, maps, sets

--- Exercise Files/src/main/kotlin/chapter4/_04_02/begin/lists.kt
+++ Exercise Files/src/main/kotlin/chapter4/_04_02/begin/lists.kt
@@ -1,5 +1,29 @@
 package chapter4._04_02.begin
 
 fun main() {
+    /**
+     * operations with lists
+     */
+
+    val names = listOf("Daisy", "Henry", "John", "Ronan")
+    println("first element: ${names.first()}")
+    println("first element: ${names[0]}")
+    var subNames = names.subList(1,3)
+    println("subNames: $subNames")
+    // names.add("Maria") compile error, should convert to mutable list
+
+    val mutableNames = names.toMutableList()
+    mutableNames.add("Maria")
+    mutableNames[0] = "Rob"
+    println("mutableNames $mutableNames")
+
+    println("sublist: ${mutableNames.subList(0,2)}") // won't include element at index 2
+
+    println("item ${mutableNames[0]} removed: ${mutableNames.removeAt(0)}")
+    println("item Sandra removed: ${mutableNames.remove("Sandra")}") // returns boolean
+    println("first name is: ${mutableNames.first()}")
+    println("last name is: ${mutableNames.last()}")
+
+
 }
 
--- Exercise Files/src/main/kotlin/chapter4/_04_05/begin/collection_operations.kt
+++ Exercise Files/src/main/kotlin/chapter4/_04_05/begin/collection_operations.kt
@@ -1,7 +1,17 @@
 package chapter4._04_05.begin
 
 fun main() {
-    val list = listOf(1, 2, 3, 4, 5, 5, 6, 7, 7, 8)
-    val set = setOf(10, 20, 30, 40, 50)
-    val map = mapOf(1 to 10, 2 to 20, 3 to 30)
+    val myList = listOf(1, 2, 3, 4, 5, 5, 6, 7, 7, 8)
+    val mySet = setOf(10, 20, 30, 40, 50)
+    val myMap = mapOf(1 to 10, 2 to 20, 3 to 30)
+
+    var tList = myList.map { element -> element * 2 }
+    println(tList)
+
+    var tMap = myMap.map { element -> Pair(element.key, element.value * 10) } // list
+    var tMap2 = myMap.map { element -> Pair(element.key, element.value * 10) }.toMap() // map
+    println(tMap)
+    println(tMap2)
+
+    // TODO continue here
 }
