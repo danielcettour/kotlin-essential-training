27ebdda1c00a05dd2e8fc36a2b989a5074925f1c
danielcettour <dcettour@jerarquicos.com>
2022-11-15 03:01:47 -0300

    07_05 companion object. Deleted trash folders

--- Exercise Files/src/main/kotlin/chapter7/07_04_class_composition.kt
+++
@@ -1,5 +0,0 @@
-package chapter7
-
-class `07_04_class_composition` {
-    // go to: chapter7/_07_04_class_composition/_07_04_class_composition.kt
-}
---
+++ Exercise Files/src/main/kotlin/chapter7/07_04_class_composition_redirect.kt
@@ -0,0 +1,7 @@
+package chapter7 // ktlint-disable filename
+
+/*
+fun main() {
+    // go to: chapter7/_07_04_class_composition/07_04_class_composition.kt
+}
+*/
